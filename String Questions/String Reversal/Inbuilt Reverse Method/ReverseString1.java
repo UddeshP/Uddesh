a741acb4ec3dd176882ac525a7d42ed4ad8efe10
UddeshP <uddeshpatole2210@gmail.com>
2024-09-08 02:32:22 +0530

    String reversal

---
+++ String Questions/String Reversal/Inbuilt Reverse Method/ReverseString1.java
@@ -0,0 +1,9 @@
+public class ReverseString1{
+
+public static void main(String [] args)
+{
+String Original="Uddesh";
+String reverse=new StringBuilder(Original).reverse().toString();
+System.out.println(reverse);
+}
+}
